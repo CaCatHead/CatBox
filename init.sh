71096ac98bb622610dee0b8b90b7d25072c3f5f8
XLor <yjl9903@vip.qq.com>
2023-01-22 19:06:14 +0800

    fix: init.sh typo

--- init.sh
+++ init.sh
@@ -6,15 +6,17 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 catj_user="${1:-catj}"
+catj_group=$(id -gn "${catj_user}")
 
 echo "Register cgroup for ${catj_user}..."
 
 ALL_SUBSYSTEMS=("cpu" "cpuacct" "memory" "pids")
 
 for subsystem in "${ALL_SUBSYSTEMS[@]}"; do
-  subsystem_dir="/sys/fs/cgroup/${catj_user}/${subsystem}/"
+  subsystem_dir="/sys/fs/cgroup/${subsystem}/${catj_user}/"
   mkdir -p "${subsystem_dir}"
   chown "${catj_user}" -R "${subsystem_dir}"
+  chgrp "${catj_group}" -R "${subsystem_dir}"
 
   if [ -d "${subsystem_dir}" ]; then
     echo "Register subsystem ${subsystem} for ${catj_user} ok"
@@ -22,6 +24,4 @@ for subsystem in "${ALL_SUBSYSTEMS[@]}"; do
     echo "Register subsystem ${subsystem} for ${catj_user} fails"
     exit 1
   fi
-
-  ls -l "/sys/fs/cgroup/${catj_user}/${subsystem}"
 done
