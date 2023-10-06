6d86670c36587ee51445c0108103caa5d3e4a04f
yjl9903 <yjl9903@onekuma.cn>
2023-10-06 14:43:04 +0800

    feat: add release script

---
+++ release.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env optc
+
+/// <reference path="/root/.optc/globals.d.ts" />
+
+export default async function(version: string) {
+  if (!/^\d+\.\d+\.\d+$/.test(version)) {
+    return;
+  }
+  
+  const toml = readTextFile('Cargo.toml');
+  writeTextFile('Cargo.toml', toml.replace(/version = "\d+\.\d+\.\d+"/, `version = "${version}"`));
+  writeTextFile('README.md', readTextFile('README.md').replace(/catj \d+\.\d+\.\d+/, `catj ${version}`));
+
+  await $`git add Cargo.toml README.md`;
+  await $`git commit -m "chore: release v${version}"`;
+  await $`git tag -a v${version} -m "chore: release v${version}"`;
+  await $`git push --tags origin main`;
+}
